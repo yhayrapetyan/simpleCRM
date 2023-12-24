ff61f444b3991fe15183885d208820e6d8ea7b5f
Yura <211003yura@gmail.com>
2023-12-24 15:13:15 +0400

    add routes[NOT finished] remove empty lines in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,19 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
-
-
         $this->call([
             AbilitySeeder::class,
             RoleSeeder::class,
             StatusSeeder::class,
             DummyDataSeeder::class,
         ]);
-
-
-
-
-
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,22 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\PasswordRecoveryController;
+use App\Http\Controllers\Client\ClientController;
+use App\Http\Controllers\Client\ClientProjectController;
+use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\Organization\OrganizationClientController;
+use App\Http\Controllers\Organization\OrganizationController;
+use App\Http\Controllers\Organization\OrganizationProjectController;
+use App\Http\Controllers\Project\ProjectController;
+use App\Http\Controllers\Project\ProjectTaskController;
+use App\Http\Controllers\Role\RoleAbilityController;
+use App\Http\Controllers\Role\RoleController;
+use App\Http\Controllers\Role\RoleUserController;
+use App\Http\Controllers\Task\TaskController;
+use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\User\UserProjectController;
+use App\Http\Controllers\User\UserTaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +30,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::redirect('/', 'login');
+//Route::redirect('/home', 'login_page');
+
+Route::view('login','auth.login')->name('login-page');
+Route::post('login', [LoginController::class, 'login'])->name('login');
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+Route::view('forgot-password', 'auth.forgot-password')->name('forgot-password');
+Route::post('send-recovery-link', [PasswordRecoveryController::class, 'SendRecoveryLink'])->name('send-recovery-link');
+Route::view('password.reset', 'password-reset');
+
+Route::middleware('auth')->group(function(){
+    Route::resource('users', UserController::class);
+    Route::resource('users.projects', UserProjectController::class)->only(['index']);
+    Route::resource('users.tasks', UserTaskController::class)->only(['index']);
+
+    Route::resource('organizations', OrganizationController::class);
+    Route::resource('organizations.clients', OrganizationClientController::class)->only(['index']);
+    Route::resource('organizations.projects', OrganizationProjectController::class)->only(['index']);
+
+
+    Route::resource('clients', ClientController::class);
+    Route::resource('clients.projects', ClientProjectController::class)->only(['index']);
+
+    Route::resource('projects', ProjectController::class);
+    Route::resource('projects.tasks', ProjectTaskController::class)->only(['index']);
+
+    Route::resource('tasks', TaskController::class);
+
+    Route::resource('roles', RoleController::class);
+    Route::resource('roles.abilities', RoleAbilityController::class)->only(['index']);
+    Route::resource('roles.users', RoleUserController::class)->only(['index']);
+
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
